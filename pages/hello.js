939b3367003445cd08c052128471411acd2bdeab
Louis CHARLES <louis.charles@gadz.org>
2022-07-11 10:37:50 -0500

    First commit

---
+++ pages/hello.js
@@ -0,0 +1,3 @@
+export default function Hello() {
+    return <div>Hello World, this is my first Next.JS App !</div>;
+};
---
+++ pages/world.js
@@ -0,0 +1,3 @@
+export default function World() {
+    return <div>This is the world page</div>;
+};
